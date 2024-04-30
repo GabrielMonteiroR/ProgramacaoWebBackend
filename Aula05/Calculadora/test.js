2171b645d1518128fdd6d25bad2e3776d16b12f7
Gabriel Mr <gmonteiro810@gmail.com>
2024-04-30 20:21:08 -0300

    message

---
+++ Aula05/Calculadora/test.js
@@ -0,0 +1,6 @@
+const {somar, subtrair, multiplicar, dividir} = require('./calculadora')
+
+console.log(somar(2,3))
+console.log(subtrair(3,5))
+console.log(multiplicar(2,3))
+console.log(dividir(10,2))
