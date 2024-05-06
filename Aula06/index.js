aa6d96af8f3ce0e937e30e1bacca03ff7b8258a5
Gabriel Mr <gmonteiro810@gmail.com>
2024-05-06 19:14:37 -0300

    TDE06.1

--- Aula06/index.js
+++ Aula06/index.js
@@ -13,9 +13,3 @@ server.get("/health", (req, res) => {
         status: "running"
     })
 })
-
-/*Crie um projeto node.
-- Configure um script de start
-- Configure um script dev utilizando o nodemon
-- Crie um servidor express que rode na porta 5000
-- Crie uma rota GET /health que retorne um JSON: {"status": "running"}*/
